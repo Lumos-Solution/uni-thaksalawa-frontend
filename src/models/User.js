802b606643cc07ebf9ef3ca840bd96b5da19800c
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-05-17 10:54:26 +0530

    update the properties in classes- add username property in user class

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,12 @@
 
 export default class User {
-  constructor(userID, name, pwd, contact, email, userType, location) {
+  constructor(userID,userName, name, pwd, contact, email, userType, location) {
     this.userID = userID;
+    this.userName=userName;
     this.name = name;
     this.pwd = pwd;//password
     this.contact = contact;
-    this.email = email;// USE AS THE userName
+    this.email = email;
     this.userType = userType;    //UNDERGRADUATE,AFTER a/L, GRADUATE LIKE THIS
     this.location = location;
   }
