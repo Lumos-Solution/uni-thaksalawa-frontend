25dbfb93151da310a6555e2665c224b3d625b38d
Poorna sayuranga <poornasayuranga@gmail.com>
2025-05-28 11:02:11 +0530

    create notification page

--- src/pages/NotificationPage.jsx
+++ src/pages/NotificationPage.jsx
@@ -1,6 +1,33 @@
-export default function NotificationPage() {
+import React from 'react';
+import NotificationCard from '../components/NotificationCard';
+
+function NotificationPage() {
+    const notifications = [
+        {
+            index: '2023001',
+            subject: 'CS101',
+            message: 'You missed the assignment submission deadline. Contact your lecturer.'
+        },
+        {
+            index: '2023002',
+            subject: 'MA201',
+            message: 'Mid-term marks have been uploaded to the LMS.'
+        },
+        {
+            index: '2023003',
+            subject: 'EN105',
+            message: 'New Zoom class scheduled for next Monday at 8 AM.'
+        }
+    ];
+
     return (
-        <>
-        </>
-    )
+        <div style={{ padding: '20px' }}>
+            <h2>My Notifications</h2>
+            {notifications.map((note, i) => (
+                <NotificationCard key={i} {...note} />
+            ))}
+        </div>
+    );
 }
+
+export default NotificationPage;
