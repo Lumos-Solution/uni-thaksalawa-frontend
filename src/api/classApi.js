a75662d0ea2c7265c92a9a883202bb34e0797edf
sawaniThiwandika <thiwandika.whs02@gmail.com>
2025-06-08 09:43:17 +0530

    add function to get All classes from backend

--- src/api/classApi.js
+++ src/api/classApi.js
@@ -1,5 +1,6 @@
 import { classes } from './mockData';
-
+import axios from "axios";
+const api = axios.create({baseURL:"http://localhost:3000/api"});
 // Function to get all classes
 export const getAllClasses = () => {
   return new Promise((resolve) => {
@@ -7,4 +8,9 @@ export const getAllClasses = () => {
       resolve(classes);
     }, 1000); // Simulate API delay
   });
+};
+
+export const fetchClasses = async () => {
+  const response = await api.get(`/classes/getAll`);
+  return response.data;
 };
